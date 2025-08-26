0db31b4734036b934111ed60b5459b3102b3020f
Willy-Tecmise <willy.oliveira@tecmise.com>
2025-08-26 14:39:31 -0300

    feat: ajustado response de classes e students

--- pkg/domain/classes/response.go
+++ pkg/domain/classes/response.go
@@ -1,17 +1,20 @@
 package classes
 
-import "github.com/tecmise/connector-school-api/pkg/domain/shared"
+import (
+	"github.com/tecmise/connector-school-api/pkg/domain/schools"
+	"github.com/tecmise/connector-school-api/pkg/domain/shared"
+)
 
 type Response struct {
-	ID       uint            `json:"id"`
-	Name     string          `json:"name"`
-	GradeID  uint            `json:"grade_id"` // ano escolar (1, 2, 3, etc)
-	Grade    shared.GradeDTO `json:"grade"`
-	ShiftID  uint            `json:"shift_id"` // turno escolar (manha, tarde ou noite)
-	Shift    shared.ShiftDTO `json:"shift"`
-	LevelID  uint            `json:"level_id"` // infantil, medio ou fundamental
-	Level    shared.LevelDTO `json:"level"`
-	SchoolID uint            `json:"school_id"`
-	// School   SchoolDTO       `json:"school"`
-	IsActive bool `json:"is_active"`
+	ID       uint             `json:"id"`
+	Name     string           `json:"name"`
+	GradeID  uint             `json:"grade_id"` // ano escolar (1, 2, 3, etc)
+	Grade    shared.GradeDTO  `json:"grade"`
+	ShiftID  uint             `json:"shift_id"` // turno escolar (manha, tarde ou noite)
+	Shift    shared.ShiftDTO  `json:"shift"`
+	LevelID  uint             `json:"level_id"` // infantil, medio ou fundamental
+	Level    shared.LevelDTO  `json:"level"`
+	SchoolID uint             `json:"school_id"`
+	School   schools.Response `json:"school"`
+	IsActive bool             `json:"is_active"`
 }
--- pkg/domain/students/response.go
+++ pkg/domain/students/response.go
@@ -1,17 +1,20 @@
 package students
 
-import "github.com/tecmise/connector-school-api/pkg/domain/shared"
+import (
+	"github.com/tecmise/connector-school-api/pkg/domain/classes"
+	"github.com/tecmise/connector-school-api/pkg/domain/shared"
+)
 
 type Response struct {
-	ID                    uint             `json:"id"`
-	PersonID              uint             `json:"person_id"`
-	Person                shared.PersonDTO `json:"person"`
-	Registration          string           `json:"registration"`
-	NameOfResponsible     string           `json:"name_of_responsible"`
-	DocumentOfResponsible string           `json:"document_of_responsible"`
-	EmailOfResponsible    string           `json:"email_of_responsible"`
-	ClassID               uint             `json:"class_id"`
-	// Class                 ClassDTO                          `json:"class"`
+	ID                    uint                              `json:"id"`
+	PersonID              uint                              `json:"person_id"`
+	Person                shared.PersonDTO                  `json:"person"`
+	Registration          string                            `json:"registration"`
+	NameOfResponsible     string                            `json:"name_of_responsible"`
+	DocumentOfResponsible string                            `json:"document_of_responsible"`
+	EmailOfResponsible    string                            `json:"email_of_responsible"`
+	ClassID               uint                              `json:"class_id"`
+	Class                 classes.Response                  `json:"class"`
 	StudentFamilyMembers  []shared.StudentFamilyMembersDTO  `json:"student_family_members"`
 	StudentMedicalRecords []shared.StudentMedicalRecordsDTO `json:"student_medical_records"`
 	StudentOccurrences    []shared.StudentOccurrencesDTO    `json:"student_occurrences"`
