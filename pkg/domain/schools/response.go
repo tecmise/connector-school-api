b10abd58eb1e97af0762ea22e9495889f916aa4c
Willy-Tecmise <willy.oliveira@tecmise.com>
2025-09-09 17:48:42 -0300

    feat: ajuste classe dto

--- pkg/domain/schools/response.go
+++ pkg/domain/schools/response.go
@@ -6,6 +6,9 @@ type Response struct {
 	ID        uint              `json:"id"`
 	Name      string            `json:"name"`
 	Type      string            `json:"type"`
+	Document  string            `json:"document"`
+	Phone     string            `json:"phone"`
+	Email     string            `json:"email"`
 	AddressID uint              `json:"address_id"`
 	Address   shared.AddressDTO `json:"address"`
 	IsActive  bool              `json:"is_active"`
--- pkg/domain/shared/class.go
+++ pkg/domain/shared/class.go
@@ -1,14 +1,20 @@
 package shared
 
-import "github.com/tecmise/connector-school-api/pkg/domain/subjects"
+import (
+	"github.com/tecmise/connector-school-api/pkg/domain/subjects"
+)
 
 type ClassSimpleDTO struct {
 	ID       uint                `json:"id"`
 	Name     string              `json:"name"`
-	GradeID  uint                `json:"grade_id"`
-	ShiftID  uint                `json:"shift_id"`
-	LevelID  uint                `json:"level_id"`
+	GradeID  uint                `json:"grade_id"` // ano escolar (1, 2, 3, etc)
+	Grade    GradeDTO            `json:"grade"`
+	ShiftID  uint                `json:"shift_id"` // turno escolar (manha, tarde ou noite)
+	Shift    ShiftDTO            `json:"shift"`
+	LevelID  uint                `json:"level_id"` // infantil, medio ou fundamental
+	Level    LevelDTO            `json:"level"`
 	SchoolID uint                `json:"school_id"`
+	School   SchoolSimpleDTO     `json:"school"`
 	IsActive bool                `json:"is_active"`
 	Subjects []subjects.Response `json:"subjects"`
 }
