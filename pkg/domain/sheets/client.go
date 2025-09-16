7bd50c1f3c85455dd812ff1de1fe745cf9c50abd
Joede Fadel <joede.fadel@tecmise.com>
2025-09-16 17:11:18 -0300

    Adds sheet domain logic
    Adds a new domain package for handling sheet-related operations.
    
    This package includes:
    - A `Client` interface defining the available sheet operations.
    - A `client` struct implementing the `Client` interface.
    - Factory functions for creating `Client` instances using either REST or Lambda outbound adapters.
    - Methods for performing CRUD operations on sheets, including listing all sheets, finding a sheet by ID, creating a new sheet, updating an existing sheet, and deleting a sheet.
    - A `Response` struct to represent the sheet data.

---
+++ pkg/domain/sheets/client.go
@@ -0,0 +1,96 @@
+package sheets
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/tecmise/connector-lib/pkg/adapters/outbound/lambda"
+	"github.com/tecmise/connector-lib/pkg/adapters/outbound/rest"
+	"github.com/tecmise/connector-lib/pkg/ports/output/connector"
+)
+
+type (
+	Client interface {
+		FindAll(ctx context.Context) (connector.ListResponse[Response], error)
+		FindByID(ctx context.Context, sheetID string) (Response, error)
+		Create(ctx context.Context, request any) (Response, error)
+		Update(ctx context.Context, sheetID string, request any) (Response, error)
+		Delete(ctx context.Context, sheetID string) (Response, error)
+	}
+
+	client struct {
+		mapper connector.Call[Response]
+		host   string
+	}
+)
+
+func Rest(host string) Client {
+	return &client{
+		host:   host,
+		mapper: rest.NewConnector[Response](),
+	}
+}
+
+func Lambda(identifier string) Client {
+	return &client{
+		host:   identifier,
+		mapper: lambda.NewConnector[Response](),
+	}
+}
+
+func (c client) FindAll(ctx context.Context) (connector.ListResponse[Response], error) {
+	var list connector.ListResponse[Response]
+	parameter := connector.NewParameterBuilder().
+		WithHost(c.host).
+		WithResource("api/sheets").
+		WithMethod("GET").
+		WithCredentials(ctx).
+		Build()
+	return list, c.mapper.Page(parameter, &list)
+}
+
+func (c client) FindByID(ctx context.Context, sheetID string) (Response, error) {
+	var sheet Response
+	parameter := connector.NewParameterBuilder().
+		WithHost(c.host).
+		WithResource(fmt.Sprintf("api/sheets/%s", sheetID)).
+		WithCredentials(ctx).
+		WithMethod("GET").
+		Build()
+	return sheet, c.mapper.Find(parameter, &sheet)
+}
+
+func (c client) Create(ctx context.Context, request any) (Response, error) {
+	var sheet Response
+	parameter := connector.NewParameterBuilder().
+		WithHost(c.host).
+		WithResource("api/sheets").
+		WithBody(request).
+		WithCredentials(ctx).
+		WithMethod("POST").
+		Build()
+	return sheet, c.mapper.Create(parameter, &sheet)
+}
+
+func (c client) Update(ctx context.Context, sheetID string, request any) (Response, error) {
+	var sheet Response
+	parameter := connector.NewParameterBuilder().
+		WithHost(c.host).
+		WithResource(fmt.Sprintf("api/sheets/%s", sheetID)).
+		WithBody(request).
+		WithCredentials(ctx).
+		WithMethod("PUT").
+		Build()
+	return sheet, c.mapper.Update(parameter, &sheet)
+}
+
+func (c client) Delete(ctx context.Context, sheetID string) (Response, error) {
+	var sheet Response
+	parameter := connector.NewParameterBuilder().
+		WithHost(c.host).
+		WithCredentials(ctx).
+		WithResource(fmt.Sprintf("api/sheets/%s", sheetID)).
+		WithMethod("DELETE").
+		Build()
+	return sheet, c.mapper.Inative(parameter, &sheet)
+}
