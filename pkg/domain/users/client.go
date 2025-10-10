14a51f0322824e5ef1abbfcd5637d54d0378fe25
joao-nk <joao.vitor@tecmise.com>
2025-10-10 12:09:04 -0300

    feat: implementacao cadastro usuario

--- pkg/domain/users/client.go
+++ pkg/domain/users/client.go
@@ -12,6 +12,10 @@ import (
 type (
 	Client interface {
 		FindUserInfo(ctx context.Context, userID string) (Response, error)
+		PaginateUsers(ctx context.Context, search string, page int, limit int, sort string) (connector.ListResponse[Response], error)
+		CreateUser(ctx context.Context, request any) (Response, error)
+		UpdateUser(ctx context.Context, request any) (Response, error)
+		InativeUser(ctx context.Context, userID string) (Response, error)
 	}
 
 	client struct {
@@ -43,4 +47,50 @@ func (c client) FindUserInfo(ctx context.Context, userID string) (Response, erro
 		WithMethod("GET").
 		Build()
 	return user, c.mapper.Find(parameter, &user)
+}
+
+func (c client) PaginateUsers(ctx context.Context, search string, page int, limit int, sort string) (connector.ListResponse[Response], error) {
+	var users connector.ListResponse[Response]
+	parameter := connector.NewParameterBuilder().
+		WithHost(c.host).
+		WithResource(fmt.Sprintf("api/users?search=%s&page=%d&page=%d&page=%s", search, page, limit, sort)).
+		WithCredentials(ctx).
+		WithMethod("GET").
+		Build()
+	return users, c.mapper.Page(parameter, &users)
+}
+
+func (c client) CreateUser(ctx context.Context, request any) (Response, error) {
+	var user Response
+	parameter := connector.NewParameterBuilder().
+		WithHost(c.host).
+		WithResource("api/users").
+		WithCredentials(ctx).
+		WithMethod("POST").
+		WithBody(request).
+		Build()
+	return user, c.mapper.Create(parameter, &user)
+}
+
+func (c client) UpdateUser(ctx context.Context, request any) (Response, error) {
+	var user Response
+	parameter := connector.NewParameterBuilder().
+		WithHost(c.host).
+		WithResource("api/users").
+		WithCredentials(ctx).
+		WithMethod("PUT").
+		WithBody(request).
+		Build()
+	return user, c.mapper.Update(parameter, &user)
+}
+
+func (c client) InativeUser(ctx context.Context, userID string) (Response, error) {
+	var user Response
+	parameter := connector.NewParameterBuilder().
+		WithHost(c.host).
+		WithResource(fmt.Sprintf("api/users/%s", userID)).
+		WithCredentials(ctx).
+		WithMethod("DELETE").
+		Build()
+	return user, c.mapper.Inative(parameter, &user)
 }
